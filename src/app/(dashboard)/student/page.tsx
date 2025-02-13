8a1a028a5be162128c1e033880599943f2f30f6a
xandersavage <47666196+xandersavage@users.noreply.github.com>
2025-02-13 23:03:48 +0100

    designed the big calendar component for the student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,4 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 const Student = () => {
-  return <div>Student</div>;
+  return (
+    <div
+      className="p-4 flex gap-4 flex-col
+        xl:flex-row"
+    >
+      {/* LEFT  */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT  */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 export default Student;
